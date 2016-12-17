7bce41307c1bf6087cc958e7bea51f566ff2ee2d
Everton Boni <evertonferreira.de@gmail.com>
2016-12-17 12:09:50 -0200

    imutabilidade e private, variaveis nao podem serem alteradas após criadas para seguranca do codigo

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,24 +1,25 @@
 class Negociacao {
     constructor(data, quantidade, valor){
 
-        this._data = data;
+        this._data = new Date(data.getTime()); //imutabilidade
         this._quantidade = quantidade;
         this._valor = valor;
+        Object.freeze(this);
     }
 
-    obtemVolume(){
+    get volume(){
         return this._quantidade * this._valor;
     }
 
-    getData(){
-        return this._data;
+    get data(){
+        return new Date(this._data.getTime()); //referencia da data baseada na negociacao, nao altera os dados após
     }
 
-    getQuantidade(){
-        return this._quantidade;
+    get quantidade(){
+        return this._quantidade; //metodo get para retornar a quantidade
     }
 
-    getValor(){
+    get valor(){
         return this._valor;
     }
 
