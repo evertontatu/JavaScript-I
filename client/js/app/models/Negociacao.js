26372b5118ebb00892c90435230824e91d881c9a
Everton Boni <evertonferreira.de@gmail.com>
2016-12-17 18:51:36 -0200

    emulando propriedades privadas e adotando por convencao o uso do _, acessando as propriedades com getters

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,10 +1,10 @@
 class Negociacao {
     constructor(data, quantidade, valor){
 
-        this._data = new Date(data.getTime()); //imutabilidade
+        this._data = new Date(data.getTime()); //imutabilidade, atributo privado
         this._quantidade = quantidade;
         this._valor = valor;
-        Object.freeze(this);
+        Object.freeze(this); //congelar o objeto após a criação, não podem alterar, shalow
     }
 
     get volume(){
@@ -12,11 +12,11 @@ class Negociacao {
     }
 
     get data(){
-        return new Date(this._data.getTime()); //referencia da data baseada na negociacao, nao altera os dados após
+        return new Date(this._data.getTime()); //referencia da data baseada na negociacao,
     }
 
     get quantidade(){
-        return this._quantidade; //metodo get para retornar a quantidade
+        return this._quantidade; //metodo get para retornar a quantidade, imutabilidade atributo privado
     }
 
     get valor(){
