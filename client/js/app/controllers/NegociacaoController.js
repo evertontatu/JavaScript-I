9cad37c31fcbe93221508085970934e8763aed14
Everton Boni <evertonferreira.de@gmail.com>
2016-12-19 18:07:00 -0200

    criando lista de negociacoes

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -7,6 +7,7 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
 
     }
 
@@ -14,14 +15,24 @@ class NegociacaoController {
 
         event.preventDefault(); //nao permite que o navegador atualize após o submit do form
 
-        let negociacao = new Negociacao(
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
+        console.log(this._listaNegociacoes.negociacoes);
+
+    }
+
+    _criaNegociacao(){
+        return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
-            this._inputValor.value
-        );
+            this._inputValor.value);
+    }
 
-        console.log(negociacao);
-        console.log(DateHelper.dataParaTexto(negociacao.data));
+    _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
 
+        this._inputData.focus();
     }
 }
