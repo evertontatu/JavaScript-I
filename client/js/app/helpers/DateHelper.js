ba1f1fd5b191ab9e58f1f11cd8e375d798b952c5
Everton Boni <evertonferreira.de@gmail.com>
2016-12-29 17:22:25 -0200

    trainning class methods

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -11,7 +11,7 @@ class DateHelper{
     static textoParaData(texto){
 
         if (!/\d{4}-\d{2}-\d{2}/.test(texto))
-            throw new Error('Deve estar no formato aaa-mm-dd');  //se o texto fornecido nao segue o padrao ano-mes-dia então exime mensagem de erro
+            throw new Error('Deve estar no formato aaaa-mm-dd');  //se o texto fornecido nao segue o padrao ano-mes-dia então exime mensagem de erro
         return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
     }
 
