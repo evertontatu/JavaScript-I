9831e44cc82a3466e84eecad80b58f9af5388f97
Everton Boni <evertonferreira.de@gmail.com>
2017-01-02 18:32:37 -0200

    learning aboult Proxy

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,11 +6,16 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+
+        /*
         this._listaNegociacoes = new ListaNegociacoes(model =>
             this._negociacoesView.update(model));
 
+        */
+
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
+
         this._mensagem = new Mensagem();
         this._mensagemView = new MensagemView($('#mensagemView'));
         this._mensagemView.update(this._mensagem);
