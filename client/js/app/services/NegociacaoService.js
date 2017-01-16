dd7b3802ac50d742f086b051dba8b7ea30601441
Everton Boni <evertonferreira.de@gmail.com>
2017-01-15 23:29:48 -0200

    Promises

--- client/js/app/services/NegociacaoService.js
+++ client/js/app/services/NegociacaoService.js
@@ -1,52 +1,29 @@
 class NegociacaoService {
 
+    construtor() {
+        this._http = new HttpService();
+    }
+
     obterNegociacoesDaSemana() {
 
         return new Promise((resolve, reject) =>{
 
-            let xhr = new XMLHttpRequest();
-
-            xhr.open('GET', 'negociacoes/semana');
-
-            xhr.onreadystatechange = () => {
-                if(xhr.readyState == 4){
-                    if(xhr.status == 200){
-
-                        resolve(JSON.parse(xhr.responseText)
-                            .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
-
-                    } else {
-                        console.log("xhr.responseText");
-                        reject('Não foi possível obter as negociações da semana');
-                    }
-                }
-            };
-            xhr.send();
+            this._http
+                .get('negociacoes/semana')
+                .then(negociacoes => {
+                    resolve(negociacoes.map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
+                })
+                .catch(err => {
+                    console.log(err);
+                    reject('Não foi possível obter as negociações da semana')
+                });
         });
     }
 
     obterNegociacoesDaSemanaAnterior() {
 
         return new Promise((resolve, reject) => {
-            let xhr = new XMLHttpRequest();
 
-            xhr.open('GET', 'negociacoes/anterior');
-
-            xhr.onreadystatechange = () => {
-                if(xhr.readyState == 4){
-                    if(xhr.status == 200){
-
-                        resolve(JSON.parse(xhr.responseText)
-                            .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
-
-                    } else {
-                        console.log("xhr.responseText");
-                        reject('Não foi possível obter as negociações da semana anterior');
-                    }
-                }
-            };
-
-            xhr.send();
         });
     }
 
