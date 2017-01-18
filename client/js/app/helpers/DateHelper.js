64e5dea701599393f6bdbbb742ebaafc04c13fc8
Everton Boni <evertonferreira.de@gmail.com>
2017-01-18 17:57:20 -0200

    cross browser for Mozilla, IE, Safari

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -10,9 +10,9 @@ class DateHelper{
 
     static textoParaData(texto){
 
-        if (!/\d{4}-\d{2}-\d{2}/.test(texto))
+        if (!/\d{2}\/\d{2}\/\d{4}/.test(texto))
             throw new Error('Deve estar no formato aaaa-mm-dd');  //se o texto fornecido nao segue o padrao ano-mes-dia então exime mensagem de erro
-        return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
+        return new Date(...texto.split('/').reverse().map((item, indice) => item - indice % 2));
     }
 
 }
