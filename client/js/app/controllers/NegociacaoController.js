36e238c04d38ef566e616646cb47bef668736390
Everton Boni <evertonferreira.de@gmail.com>
2017-01-20 16:15:53 -0200

    install Babel and exc build

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,20 +1,22 @@
-class NegociacaoController {
+'use strict';
 
-    constructor() {
-        let $ = document.querySelector.bind(document); //atribuindo ao $ a funcao de executar o querySelector ainda respondendo a document e nao ao contexto do $
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var NegociacaoController = function () {
+    function NegociacaoController() {
+        _classCallCheck(this, NegociacaoController);
+
+        var $ = document.querySelector.bind(document); //atribuindo ao $ a funcao de executar o querySelector ainda respondendo a document e nao ao contexto do $
 
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
 
-        this._listaNegociacoes = new Bind(
-            new ListaNegociacoes(),
-            new NegociacoesView($('#negociacoesView')),
-            'adiciona', 'esvazia', 'ordena', 'inverteOrdem');
+        this._listaNegociacoes = new Bind(new ListaNegociacoes(), new NegociacoesView($('#negociacoesView')), 'adiciona', 'esvazia', 'ordena', 'inverteOrdem');
 
-        this._mensagem = new Bind(
-            new Mensagem(), new MensagemView($('#mensagemView')),
-            'texto');
+        this._mensagem = new Bind(new Mensagem(), new MensagemView($('#mensagemView')), 'texto');
 
         this._ordemAtual = '';
 
@@ -23,76 +25,93 @@ class NegociacaoController {
         this._init();
     }
 
-    _init() {
+    _createClass(NegociacaoController, [{
+        key: '_init',
+        value: function _init() {
+            var _this = this;
 
-        this._service
-        .lista()
-        .then(negociacoes =>
-                negociacoes.forEach(negociacao =>
-                    this._listaNegociacoes.adiciona(negociacao)))
-            .catch(err => this._mensagem.texto = err);
+            this._service.lista().then(function (negociacoes) {
+                return negociacoes.forEach(function (negociacao) {
+                    return _this._listaNegociacoes.adiciona(negociacao);
+                });
+            }).catch(function (err) {
+                return _this._mensagem.texto = err;
+            });
 
-        setInterval(() => {
-            this.importaNegociacoes();
-        }, 3000);
-    }
-
-    adiciona(event) {
-        event.preventDefault();
-
-        let negociacao = this._criaNegociacao();
-
-        this._service
-            .cadastra(negociacao)
-            .then(mensagem => {
-                this._listaNegociacoes.adiciona(negociacao);
-                this._mensagem.texto = mensagem;
-                this._limpaFormulario();
-            })
-            .catch(err => this._mensagem.texto = err);
-    }
-
-    importaNegociacoes() {
-
-        this._service
-            .importa(this._listaNegociacoes.negociacoes)
-            .then(negociacoes => negociacoes.forEach(negociacao => {
-                this._listaNegociacoes.adiciona(negociacao);
-                this._mensagem.texto = 'Negociações do período importadas'
-            }))
-            .catch(err => this._mensagem.texto = err);
-    }
-
-    ordena(coluna) {
-        if(this._ordenaAtual == coluna) {
-            this._listaNegociacoes.inverteOrdem();
-        } else {
-            this._listaNegociacoes.ordena((a, b) => a[coluna] - b[coluna]);
+            setInterval(function () {
+                _this.importaNegociacoes();
+            }, 3000);
         }
-        this._ordemAtual = coluna;
-    }
+    }, {
+        key: 'adiciona',
+        value: function adiciona(event) {
+            var _this2 = this;
 
-    apaga() {
-        this._service
-            .apaga()
-            .then(mensagem => {
-                this._mensagem.texto = mensagem;
-                this._listaNegociacoes.esvazia();
-            })
-            .catch(err => this.mensagem.texto = err);
-    }
+            event.preventDefault();
 
-    _criaNegociacao() {
-        return new Negociacao(
-            DateHelper.textoParaData(this._inputData.value),
-            parseInt(this._inputQuantidade.value),
-            parseFloat(this._inputValor.value));
-    }
+            var negociacao = this._criaNegociacao();
 
-    _limpaFormulario() {
-        this._inputData.value = '';
-        this._inputQuantidade.value = 1;
-        this._inputValor.value = 0.0;
-        this._inputData.focus();
-    }
-}
+            this._service.cadastra(negociacao).then(function (mensagem) {
+                _this2._listaNegociacoes.adiciona(negociacao);
+                _this2._mensagem.texto = mensagem;
+                _this2._limpaFormulario();
+            }).catch(function (err) {
+                return _this2._mensagem.texto = err;
+            });
+        }
+    }, {
+        key: 'importaNegociacoes',
+        value: function importaNegociacoes() {
+            var _this3 = this;
+
+            this._service.importa(this._listaNegociacoes.negociacoes).then(function (negociacoes) {
+                return negociacoes.forEach(function (negociacao) {
+                    _this3._listaNegociacoes.adiciona(negociacao);
+                    _this3._mensagem.texto = 'Negociações do período importadas';
+                });
+            }).catch(function (err) {
+                return _this3._mensagem.texto = err;
+            });
+        }
+    }, {
+        key: 'ordena',
+        value: function ordena(coluna) {
+            if (this._ordenaAtual == coluna) {
+                this._listaNegociacoes.inverteOrdem();
+            } else {
+                this._listaNegociacoes.ordena(function (a, b) {
+                    return a[coluna] - b[coluna];
+                });
+            }
+            this._ordemAtual = coluna;
+        }
+    }, {
+        key: 'apaga',
+        value: function apaga() {
+            var _this4 = this;
+
+            this._service.apaga().then(function (mensagem) {
+                _this4._mensagem.texto = mensagem;
+                _this4._listaNegociacoes.esvazia();
+            }).catch(function (err) {
+                return _this4.mensagem.texto = err;
+            });
+        }
+    }, {
+        key: '_criaNegociacao',
+        value: function _criaNegociacao() {
+            return new Negociacao(DateHelper.textoParaData(this._inputData.value), parseInt(this._inputQuantidade.value), parseFloat(this._inputValor.value));
+        }
+    }, {
+        key: '_limpaFormulario',
+        value: function _limpaFormulario() {
+            this._inputData.value = '';
+            this._inputQuantidade.value = 1;
+            this._inputValor.value = 0.0;
+            this._inputData.focus();
+        }
+    }]);
+
+    return NegociacaoController;
+}();
+//# sourceMappingURL=NegociacaoController.js.map
